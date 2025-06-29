2269f90b3e2de03c353c855d51bd1e46af0d47e8
Odin Bartusiak <dev.odin.bartusiak@gmail.com>
2025-06-29 19:09:52 +0200

    correct

--- sw.js
+++ sw.js
@@ -13,19 +13,30 @@ const caches_url = [
   self.location.origin + "/workout/exercices/index.html",
   self.location.origin + "/workout/exercices/UX.css",
   self.location.origin + "/workout/exercices/cpu.js",
-  "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
-  "https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap",
-  "https://fonts.gstatic.com/s/materialsymbolsoutlined/v254/kJEhBvYX7BgnkSrUwT8OhrdQw4oELdPIeeII9v6oFsI.woff2",
-  "https://fonts.gstatic.com/s/opensans/v43/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTS-muw.woff2",
-  "https://fonts.gstatic.com/s/ebgaramond/v31/SlGUmQSNjdsmc35JDF1K5GR1SDk.woff2",
   self.location.origin + "/manifest.json",
   self.location.origin + "/icons/192.png",
   self.location.origin + "/icons/512.png",
 
 ];
 
+function cacheOrFetch(request){
+  return caches.match(request).then(function(response){
+    if(!response){
+      return fetch(request).then(function cacheRequest(response){
+        const cache_res = response.clone();
+        caches.open("v1").then(function(cache){
+          cache.put(request, cache_res);
+        });
+
+        return response;
+      })
+    }
+    return response
+  });
+}
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(cacheOrFetch(event.request));
 });
 
 self.addEventListener("activate", function(event){
