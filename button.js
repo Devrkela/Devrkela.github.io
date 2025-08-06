925a15e3a7b84a88a97472e591ceb42d273b9d40
Odin Bartusiak <dev.odin.bartusiak@gmail.com>
2025-08-06 19:49:35 +0200

    on pointer down

--- button.js
+++ button.js
@@ -5,17 +5,14 @@ for(let button of document.querySelectorAll("button")){
   pressed_element.style.width = 2 * dimension.width + dimension.height;
   pressed_element.style.height = 2 * dimension.width + dimension.height;      
 
-  button.addEventListener("pointermove", function move(event){
-    const dimension = this.querySelector(".decoration").getBoundingClientRect();
+  button.addEventListener("pointerdown" , function pressed(event){
+        const dimension = this.querySelector(".decoration").getBoundingClientRect();
 
     const percent_x = event.clientX - dimension.x - pressed_element.clientWidth/2;
     const percent_y = event.clientY - dimension.y - pressed_element.clientHeight/2;
 
     pressed_element.style.left = percent_x;
     pressed_element.style.top  = percent_y;
-  });
-  
-  button.addEventListener("pointerdown" , function pressed(event){
     pressed_element.style.transition = "0ms";
     pressed_element.style.transform = null;
     pressed_element.style.opacity = null;
