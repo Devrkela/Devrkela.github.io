34fed453058a48353c9763a1763bcfe0614535ff
Odin Bartusiak <dev.odin.bartusiak@gmail.com>
2025-08-11 18:52:38 +0200

    group remove

--- groups.js
+++
@@ -1,6 +0,0 @@
-for(let button of document.querySelectorAll("button")){
-  button.addEventListener("pointerdown", function(){
-    if(this.getAttribute("aria-pressed") === "true") return this.setAttribute("aria-pressed", "false");
-    return this.setAttribute("aria-pressed", "true");
-  })
-}
